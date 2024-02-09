e9336c5694593d84c83ca30f94ab4223f884a95b
C856k <Carl856k@stud.kea.dk>
2024-02-09 12:13:36 +0100

    Added .gitignore and worked on the exercise

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+function binarySearch(arr, variant) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid].variant === variant) {
+            return mid;
+        } else if (arr[mid].variant < variant) {
+            left = mid + 1;
+        } else {
+            right = mid -1;
+        }
+    }
+    return null;
+}
+
+const response = await fetch("ddo-fullform\ddo_fullforms_2023-10-11.csv");
+const rawtext = await response.text();
+
+let globalArrayOfWords = rawtext.split("\n").map(line => {
+    const parts = line.split("\t");
+    return {
+        variant: parts[0],
+        headword: parts[1],
+        homograph: parts[2],
+        partofspeech: parts[3],
+        id: parts[4]
+    };
+});
+
+const searchTerm = "hestevogn";
+const foundWord = binarySearch(globalArrayOfWords, searchTerm);
+if (foundWord) {
+    console.log(JSON.stringify(foundWord, null, 2));
+} else {
+    console.log(`'${searchTerm}' not found`);
+}
